0edee7dc201455fefda28889b7cca6497d898211
Uma Negi <umanegi267@gmail.com>
2021-10-09 18:06:17 +0530

    Custom ErrorResponse class

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require('../models/Bootcamp');
+const ErrorResponse = require('../utils/errorResponse');
 
 
 //@desc      Get all bootcamps
@@ -12,9 +13,9 @@ exports.getBootcamps = async (req, res, next) => {
         res.status(200).json({ success: true, data: bootcamps });
 
     } catch (err) {
-        res.status(400).json({success: false});
+        res.status(400).json({ success: false });
     }
-    
+
 };
 
 
@@ -22,19 +23,23 @@ exports.getBootcamps = async (req, res, next) => {
 //@route     GET /api/v1/bootcamps/:id
 //@access    Public
 exports.getBootcamp = async (req, res, next) => {
-    
+
     try {
         const bootcamp = await Bootcamp.findById(req.params.id);
 
-        if(!bootcamp){
-           return res.status(400).json({success: false});
+        if (!bootcamp) {
+            return next(
+                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+            );
         }
-        
-        res.status(200).json({success: true, data: bootcamp});
-        
+
+        res.status(200).json({ success: true, data: bootcamp });
+
     } catch (err) {
         // res.status(400).json({success: false});
-        next(err);
+        next(
+            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+        );
     }
 };
 
@@ -74,16 +79,16 @@ exports.updateBootcamp = async (req, res, next) => {
             runValidators: true
         });
 
-        if(!bootcamp){
-            return res.status(400).json({success: false});
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
         }
 
-        res.status(200).json({success: true, data: bootcamp});
+        res.status(200).json({ success: true, data: bootcamp });
 
     } catch (err) {
-        res.status(400).json({success: false});
+        res.status(400).json({ success: false });
     }
-    
+
 };
 
 
@@ -95,14 +100,14 @@ exports.deleteBootcamp = async (req, res, next) => {
     try {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
-        if(!bootcamp){
-           return res.status(400).json({success: false});
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
         }
 
-        res.status(200).json({success: true, data: {} });
+        res.status(200).json({ success: true, data: {} });
 
     } catch (err) {
-        res.status(400).json({success: false});
+        res.status(400).json({ success: false });
     }
-    
+
 };
