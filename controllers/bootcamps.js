ecb67a227835785e690fb563000fdf009bc41d0e
Uma Negi <umanegi267@gmail.com>
2021-10-11 20:00:05 +0530

    Mongoose Error Handling 1 (CastError / Mongoose Bad ObjectId)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -13,7 +13,7 @@ exports.getBootcamps = async (req, res, next) => {
         res.status(200).json({ success: true, data: bootcamps });
 
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 
 };
@@ -37,9 +37,7 @@ exports.getBootcamp = async (req, res, next) => {
 
     } catch (err) {
         // res.status(400).json({success: false});
-        next(
-            new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-        );
+        next(err);
     }
 };
 
@@ -59,9 +57,7 @@ exports.createBootcamp = async (req, res, next) => {
         });
 
     } catch (err) {
-        res.status(400).json({
-            success: false
-        });
+        next(err);
     }
 
 
@@ -80,13 +76,15 @@ exports.updateBootcamp = async (req, res, next) => {
         });
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(
+                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+            );
         }
 
         res.status(200).json({ success: true, data: bootcamp });
 
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 
 };
@@ -101,13 +99,15 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false });
+            return next(
+                new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+            );
         }
 
         res.status(200).json({ success: true, data: {} });
 
     } catch (err) {
-        res.status(400).json({ success: false });
+        next(err);
     }
 
 };
