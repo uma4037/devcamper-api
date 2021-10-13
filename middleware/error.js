59bcd7cfb48401cabec78d3e0bd1be076302d731
Uma Negi <umanegi267@gmail.com>
2021-10-13 14:00:37 +0530

    Mongoose Error Handling 2 (Mongoose Duplicate key, Validation Error)

--- middleware/error.js
+++ middleware/error.js
@@ -5,7 +5,9 @@ const errorHandler = (err, req, res, next) => {
     error.message = err.message;
 
     //Log to console for dev
-    console.log(err.stack.red);
+    //console.log(err.stack.red);
+    console.log(err);
+
     //console.log(err.name);
 
     //Mongoose bad ObjectId
@@ -13,6 +15,20 @@ const errorHandler = (err, req, res, next) => {
         const message = `Bootcamp not found with id of ${err.value}`;
 
         error = new ErrorResponse(message, 404);
+    }
+
+    //Mongoose duplicate key
+    if(err.code === 11000){
+        const message = `Duplicate field value entered`;
+
+        error = new ErrorResponse(message, 400);
+    }
+
+    //Mongoose Validation Error
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => val.message);
+
+        error = new ErrorResponse(message, 400);
     }
 
     res.status( error.statusCode || 500).json({
