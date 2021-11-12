f1890ce5d0a22eb8aefc296cad5f4e083653cd22
Uma Negi <umanegi267@gmail.com>
2021-11-12 22:16:59 +0530

    Forgot Password - Generate token

--- models/User.js
+++ models/User.js
@@ -1,9 +1,11 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { userInfo } = require('os');
 
 const UserSchema = new mongoose.Schema({
-    name:{
+    name: {
         type: String,
         required: [true, 'Please add a name']
     },
@@ -12,8 +14,8 @@ const UserSchema = new mongoose.Schema({
         required: [true, 'Please add an email'],
         unique: true,
         match: [
-          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-          'Please add a valid email'
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+            'Please add a valid email'
         ]
     },
     role: {
@@ -37,23 +39,45 @@ const UserSchema = new mongoose.Schema({
 
 
 //Encrypt password using bcrypt
-UserSchema.pre('save', async function(next){
+UserSchema.pre('save', async function (next) {
+    if(!this.isModified('password')){
+        next();
+    }
+
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
 
 
 //Sign JWT and return
-UserSchema.methods.getSignedJwtToken = function(){
-    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE
     });
 };
 
 
 //Match user entered password to hashed password in database
-UserSchema.methods.matchPassword = async function(enteredPassword){
+UserSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
+};
+
+
+//Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+    //Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    //Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    //Set expire
+    this.resetPasswordExpire = Date.now() + 10*60*1000;    
+
+    return resetToken;
 };
 
 
