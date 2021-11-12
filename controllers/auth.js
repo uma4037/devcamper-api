075cc67e7b010f4a6aefc11a5f80aa7689693137
Uma Negi <umanegi267@gmail.com>
2021-11-12 23:15:01 +0530

    Forgot Password - Send Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 
 //@desc      Register user
 //@route     POST /api/v1/auth/register
 //@access    Public
-exports.register = asyncHandler(async(req, res, next) => {
-    const {name, email, password, role} = req.body;
+exports.register = asyncHandler(async (req, res, next) => {
+    const { name, email, password, role } = req.body;
 
     //Create user
     const user = await User.create({
@@ -24,20 +25,20 @@ exports.register = asyncHandler(async(req, res, next) => {
 //@desc      Login user
 //@route     POST /api/v1/auth/login
 //@access    Public
-exports.login = asyncHandler(async(req, res, next) => {
+exports.login = asyncHandler(async (req, res, next) => {
     const { email, password } = req.body;
 
 
     //Validate email & password
-    if(!email || !password){
+    if (!email || !password) {
         return next(new ErrorResponse('Please provide an email and password', 400));
     }
 
 
     //Check for user
-    const user = await User.findOne({email}).select('+password');
+    const user = await User.findOne({ email }).select('+password');
 
-    if(!user){
+    if (!user) {
         return next(new ErrorResponse('Invalid Credentials', 401));
     }
 
@@ -45,7 +46,7 @@ exports.login = asyncHandler(async(req, res, next) => {
     //Check if password matches
     const isMatch = await user.matchPassword(password);
 
-    if(!isMatch){
+    if (!isMatch) {
         return next(new ErrorResponse('Invalid Credentials', 401));
     }
 
@@ -57,7 +58,7 @@ exports.login = asyncHandler(async(req, res, next) => {
 //@desc      Get current logged in user
 //@route     GET /api/v1/auth/me
 //@access    Private
-exports.getMe = asyncHandler(async(req, res, next) => {
+exports.getMe = asyncHandler(async (req, res, next) => {
     const user = await User.findById(req.user.id);
 
     res.status(200).json({
@@ -70,10 +71,10 @@ exports.getMe = asyncHandler(async(req, res, next) => {
 //@desc      Forgot password
 //@route     POST /api/v1/auth/forgotpassword
 //@access    Public
-exports.forgotPassword = asyncHandler(async(req, res, next) => {
-    const user = await User.findOne({email: req.body.email});
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
 
-    if(!user){
+    if (!user) {
         return next(
             new ErrorResponse('There is no user with that email', 404)
         );
@@ -83,12 +84,33 @@ exports.forgotPassword = asyncHandler(async(req, res, next) => {
     const resetToken = user.getResetPasswordToken();
 
     //console.log(resetToken);
-    await user.save({validateBeforeSave: false});
+    await user.save({ validateBeforeSave: false });
 
-    res.status(200).json({
-        success: true,
-        data: user
-    });
+    //Create reset url
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/auth/resetpassword/${resetToken}`;
+
+    const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Password reset token',
+            message
+        });
+
+        res.status(200).json({success: true, data: 'Email sent'});
+    } catch (err) {
+        console.log(err);
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save({validateBeforeSave: false});
+
+        return next(
+            new ErrorResponse('Email could not be sent', 500)
+        );
+    }
+
 });
 
 
@@ -99,19 +121,19 @@ const sendTokenResponse = (user, statusCode, res) => {
     const token = user.getSignedJwtToken();
 
     const options = {
-        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE *24*60*60*1000),
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true
     }
 
-    if(process.env.NODE_ENV === 'production'){
+    if (process.env.NODE_ENV === 'production') {
         options.secure = true;
     }
 
     res
-      .status(statusCode)
-      .cookie('token', token, options)
-      .json({
-          success: true,
-          token
-      });
+        .status(statusCode)
+        .cookie('token', token, options)
+        .json({
+            success: true,
+            token
+        });
 };
