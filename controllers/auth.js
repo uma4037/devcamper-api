910ce584ac33e65070253c1fef6c440a2fbd24c8
Uma Negi <umanegi267@gmail.com>
2021-11-14 12:35:04 +0530

    Reset Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
@@ -98,19 +99,49 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
             message
         });
 
-        res.status(200).json({success: true, data: 'Email sent'});
+        res.status(200).json({ success: true, data: 'Email sent' });
     } catch (err) {
         console.log(err);
         user.resetPasswordToken = undefined;
         user.resetPasswordExpire = undefined;
 
-        await user.save({validateBeforeSave: false});
+        await user.save({ validateBeforeSave: false });
 
         return next(
             new ErrorResponse('Email could not be sent', 500)
         );
     }
 
+});
+
+
+//@desc      Reset password
+//@route     PUT /api/v1/auth/resetpassword/:resettoken
+//@access    Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+    //Get hashed token
+    const resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(req.params.resettoken)
+        .digest('hex');
+    
+
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    });
+
+    if(!user){
+        return next(new ErrorResponse('Invalid token', 400));
+    }
+
+    //Set new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
 });
 
 
